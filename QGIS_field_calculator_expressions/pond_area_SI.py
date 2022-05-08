97402e37b92c16a531e9e39f36f86ea7e4cb8793
RGreensmith <rose.greensmith@yahoo.co.uk>
2022-05-08 10:52:27 +0100

    add pond_area_SI QGIS field calculator expression function

---
+++ QGIS_field_calculator_expressions/pond_area_SI.py
@@ -0,0 +1,39 @@
+from qgis.core import *
+from qgis.gui import *
+
+def get_m_c(coord1,coord2):
+    dy = coord2['y']-coord1['y']
+    dx = coord2['x']-coord1['x']
+    
+    m = dy/dx
+    mx = m*coord1['x']
+    c = coord1['y']-mx
+    
+    return (m,c)
+
+
+def get_SI(coord1,coord2,pond_area):
+    m,c = get_m_c(coord1,coord2)
+    SI = m*pond_area+c
+    
+    return SI
+
+
+@qgsfunction(args='auto', group='Custom', referenced_columns=['pond_area'])
+def pond_area_SI(feature, parent):
+    pond_area = feature.attribute('pond_area')
+    
+    if (pond_area < 500):
+        coord1 = {'x':0,'y':0}
+        coord2 = {'x':400,'y':0.8}
+        return get_SI(coord1,coord2,pond_area)
+    
+    if (pond_area <700):
+        return feature.attribute(1)
+        
+    if (pond_area < 2000):
+        coord1 = {'x':700,'y':1}
+        coord2 = {'x':2000,'y':0.8}
+        return get_SI(coord1,coord2,pond_area)
+        
+    return feature.attribute(-9999)
